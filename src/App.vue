e2e0f79fb188942e09d5748954ca6cce0c7abb37
Eldar-X <eldarx@gmail.com>
2021-08-15 14:46:56 +0300

    latest

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-dialog persistent :model-value="true">
+    <div class="row">
+      <div class="col-12">
+        <q-input label="With v-model(Not-Updatable)" v-model="inputValue" />
+      </div>
+      <div class="col-12">
+        <q-input label="Without v-model(Updatable)"></q-input>
+      </div>
+    </div>
+  </q-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+  setup() {
+    const inputValue = ref("");
+    return {
+      inputValue,
+    };
+  },
+});
+</script>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { Quasar } from "quasar";
+import "quasar/dist/quasar.prod.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).use(Quasar).mount("#app");
